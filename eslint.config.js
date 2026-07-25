a107f5873b2108c7bb2b18d5eedc2335e503d1b1
shahramRezvani <53124952+shahramRezvani@users.noreply.github.com>
2026-07-25 20:56:14 +0330

    chore: add eslint.config.js extending next/core-web-vitals and next/typescript

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'next/typescript',
+  ],
+  // You can add custom rules here if needed
+};
